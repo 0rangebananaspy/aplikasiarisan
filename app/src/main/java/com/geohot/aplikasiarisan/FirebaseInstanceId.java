527b1ed10a300b1c838e8aeec9e3b5565fecad97
0rangebananaspy <h@unej.ac.id>
2022-04-16 00:50:06 +0700

    Update Beta 1

---
+++ app/src/main/java/com/geohot/aplikasiarisan/FirebaseInstanceId.java
@@ -0,0 +1,93 @@
+package com.geohot.aplikasiarisan;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FirebaseInstanceId extends FirebaseMessagingService {
+    private static final String TAG = "FCM Service";
+    PendingIntent pendingIntent;
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+//            if (/* Check if data needs to be processed by long running job */ true) {
+//                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+//                scheduleJob();
+//            } else {
+//                // Handle message within 10 seconds
+//                handleNow();
+//            }
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            notifikasi(remoteMessage.getNotification().getBody(), remoteMessage.getNotification().getTitle());
+        }
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+
+//        Log.e(TAG, "Refreshed token: " + s);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(s);
+    }
+    private void sendRegistrationToServer(String s) {
+//        Log.e("TAG", s);
+
+    }
+    public void notifikasi(String title, String body){
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent =PendingIntent.getActivity(this,
+                0,intent,PendingIntent.FLAG_ONE_SHOT);
+        String channelId = "ID";
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder;
+        notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setContentTitle(title)
+                        .setContentText(body)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setStyle(new NotificationCompat.InboxStyle())
+                        .setContentIntent(pendingIntent);
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+// Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
